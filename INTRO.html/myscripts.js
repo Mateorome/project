b86832ae5535eb252bfeb66c50c82e45210f621a
Diego <airbitco@gmail.com>
2023-09-09 10:41:55 -0500

    adding the first javascript file

---
+++ INTRO.html/myscripts.js
@@ -0,0 +1,12 @@
+let a = 1230;
+let b = 897;
+
+if (b < a) {
+    setTimeout(() => {
+        alert("el numero menor es: " + b);
+    },4000);
+} else {
+    setTimeout(() => {
+        alert("el numero mayor es: " + a);
+    },4000);
+}
